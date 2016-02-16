5117bb58e672aa2ac46da1f3bf554bc4206799ea
teamThevibe <team.thevibe@gmail.com>
2016-02-16 23:59:28 +0200

    test

--- autoupdate.cmd
+++ autoupdate.cmd
@@ -1,7 +1,6 @@
-C:\GitHub\repository.thevibe\Repo\repo_update.py
 git config remote.origin.url https://teamthevibe:kodiforall2015@github.com/teamThevibe/repository.thevibe.git
 git pull
 git add *
 git commit -m "test"
-git push
+git push origin
 pause
